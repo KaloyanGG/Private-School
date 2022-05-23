4453af0c3b1839a804cff3088a855223735f78d6
Koko <kokicha.gg@gmail.com>
2022-05-23 19:00:20 +0300

    Create StaticData.cs
    [StaticData] Add Roles.

---
+++ PrivateSchool/Data/StaticData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Data
+{
+    public static class StaticData
+    {
+        public static Dictionary<int, string> Roles = new Dictionary<int, string>() { { 0, "Student" }, { 1, "Teacher" } };
+    }
+}
