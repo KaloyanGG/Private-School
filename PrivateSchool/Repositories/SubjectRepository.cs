207c70ee62a1a5f10da7a3e29de7e5cc92e752e9
Koko <kokicha.gg@gmail.com>
2022-05-29 21:19:03 +0300

    Create SubjectRepository.cs
    [SubjectRepository] The implementation of the interface.

---
+++ PrivateSchool/Repositories/SubjectRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrivateSchool.Data;
+using PrivateSchool.Entities;
+using PrivateSchool.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Repositories
+{
+    public class SubjectRepository : ISubjectRepository
+    {
+        private readonly PrivateSchoolDBContext _db;
+
+        public SubjectRepository(PrivateSchoolDBContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<Subject>> GetAllSubjects()
+        {
+            return await _db.Subjects.ToListAsync();
+        }
+    }
+}
