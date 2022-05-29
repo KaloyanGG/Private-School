7fbcb4c8789963d47b1c1a71cd25554a1518ff8d
Koko <kokicha.gg@gmail.com>
2022-05-29 21:18:36 +0300

    Create ISubjectRepository.cs
    [ISubjectRepository] Subject Repository interface with retrieving all subjects.

---
+++ PrivateSchool/Repositories/Interfaces/ISubjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using PrivateSchool.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Repositories.Interfaces
+{
+    public interface ISubjectRepository
+    {
+        Task<List<Subject>> GetAllSubjects();
+    }
+}
