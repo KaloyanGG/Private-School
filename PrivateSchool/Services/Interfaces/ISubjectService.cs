e17b84644a5f5d200d396744082eac940c862826
Koko <kokicha.gg@gmail.com>
2022-05-29 21:19:40 +0300

    Service interface and implementation
    [SubjectService] Service for working with subjects.

---
+++ PrivateSchool/Services/Interfaces/ISubjectService.cs
@@ -0,0 +1,14 @@
+﻿using PrivateSchool.Entities;
+using PrivateSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Services.Interfaces
+{
+    public interface ISubjectService
+    {
+        Task<List<Subject>> GetAllSubjects();
+    }
+}
---
+++ PrivateSchool/Services/SubjectService.cs
@@ -0,0 +1,28 @@
+﻿using PrivateSchool.Entities;
+using PrivateSchool.Models;
+using PrivateSchool.Repositories;
+using PrivateSchool.Repositories.Interfaces;
+using PrivateSchool.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Services
+{
+    public class SubjectService : ISubjectService
+    {
+        private readonly ISubjectRepository _subjectRepository;
+
+        public SubjectService(ISubjectRepository subjectRepository)
+        {
+            _subjectRepository = subjectRepository;
+        }
+        public async Task<List<Subject>> GetAllSubjects()
+        {
+            List<Subject> result = await _subjectRepository.GetAllSubjects();
+            
+            return result;
+        }
+    }
+}
