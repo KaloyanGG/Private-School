e288eda50e6dd5f7b0253b8bc428cacaa81410fc
Koko <kokicha.gg@gmail.com>
2022-05-29 21:18:00 +0300

    Create SubjectReturnModel.cs
    [SubjectReturnModel] Added in case if needed.

---
+++ PrivateSchool/Models/SubjectReturnModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Models
+{
+    public class SubjectReturnModel
+    {
+        public int Id { get; set; }
+        public int MaxCapacity { get; set; }
+        public string Name { get; set; }
+
+    }
+}
