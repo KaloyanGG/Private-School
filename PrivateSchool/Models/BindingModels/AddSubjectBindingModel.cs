1088d8a63bab346299cb7b37b7eba4b6a98a80bd
Koko <kokicha.gg@gmail.com>
2022-05-31 12:39:27 +0300

    Create AddSubjectBindingModel.cs
    [AddSubjectBindingModel] Model for the UI

---
+++ PrivateSchool/Models/BindingModels/AddSubjectBindingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Models.BindingModels
+{
+    public class AddSubjectBindingModel
+    {
+
+        public string Name { get; set; }
+        public int MaxCapacity { get; set; }
+
+    }
+}
