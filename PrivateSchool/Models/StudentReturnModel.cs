54028e6b1e9878a322fecfd1b47196486b60c9b2
Koko <kokicha.gg@gmail.com>
2022-06-01 18:21:07 +0300

    Create StudentReturnModel.cs
    [StudentReturnModel] New model for returning a student

---
+++ PrivateSchool/Models/StudentReturnModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Models
+{
+    public class StudentReturnModel
+    {
+
+        public string Username { get; set; }
+        public int Id { get; set; }
+
+
+
+    }
+}
