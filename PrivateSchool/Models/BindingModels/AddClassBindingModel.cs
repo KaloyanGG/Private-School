3bf71630b42a043ae390cec6a6a6ca710d8916e4
Koko <kokicha.gg@gmail.com>
2022-06-01 18:19:53 +0300

    Update AddClassBindingModel.cs
    [AddClassBindingModel] removed SubjectId

--- PrivateSchool/Models/BindingModels/AddClassBindingModel.cs
+++ PrivateSchool/Models/BindingModels/AddClassBindingModel.cs
@@ -8,7 +8,6 @@ namespace PrivateSchool.Models.BindingModels
     public class AddClassBindingModel
     {
         public int TeacherId { get; set; }
-        public int SubjectId { get; set; }
         public string Name { get; set; }
 
     }
