5f44e50598f3beeda3987b8f71508985274bf1e5
Koko <kokicha.gg@gmail.com>
2022-06-01 18:20:51 +0300

    More information models
    [UsersModels] Added models with more info for student, teacher and user

---
+++ PrivateSchool/Models/FullInfoStudentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Models
+{
+    public class FullInfoStudentModel :FullInfoUserReturnModel
+    {
+        public double? AverageGrade { get; set; }
+
+    }
+}
---
+++ PrivateSchool/Models/FullInfoTeacherModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Models
+{
+    public class FullInfoTeacherModel : FullInfoUserReturnModel
+    {
+        public int? Level { get; set; }
+    }
+}
