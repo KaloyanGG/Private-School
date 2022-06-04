351ad24753f28e2b928ea649ee3d899a659c57ce
Koko <kokicha.gg@gmail.com>
2022-06-04 19:09:18 +0300

    [FullInfoUserReturnModel] Added egn, email, dateofbirth and role

--- PrivateSchool/Models/FullInfoUserReturnModel.cs
+++ PrivateSchool/Models/FullInfoUserReturnModel.cs
@@ -11,7 +11,10 @@ namespace PrivateSchool.Models
        // public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-      //  public string StudentOrTeacher { get; set; }
+        public string EGN { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Role { get; set; }
 
         
 
