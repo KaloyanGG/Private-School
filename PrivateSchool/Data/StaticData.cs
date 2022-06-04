5434fe7cf9958e08d3eafa59b5938f2e1baeaed8
Koko <kokicha.gg@gmail.com>
2022-06-04 19:06:56 +0300

    [RolesData] Reversing the roles from Dictionary to string array

--- PrivateSchool/Data/StaticData.cs
+++ PrivateSchool/Data/StaticData.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PrivateSchool.Data
+﻿namespace PrivateSchool.Data
 {
     public static class StaticData
     {
-        public static Dictionary<int, string> Roles = new Dictionary<int, string>() { { 0, "Student" }, { 1, "Teacher" } };
+        public static readonly string[] Roles = { "Student",  "Teacher" };
     }
 }
