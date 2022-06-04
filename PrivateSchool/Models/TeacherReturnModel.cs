24286a28332b2d857708ef93be671672eaf18fc7
Koko <kokicha.gg@gmail.com>
2022-06-04 19:09:25 +0300

    Create TeacherReturnModel.cs

---
+++ PrivateSchool/Models/TeacherReturnModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateSchool.Models
+{
+    public class TeacherReturnModel
+    {
+        public string Username { get; set; }
+        public int Level { get; set; }
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+
+    }
+}
