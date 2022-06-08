02fe6301af93a8e47a5e38f32d7b74ac40e399b2
Koko <kokicha.gg@gmail.com>
2022-06-08 09:24:38 +0300

    [AutoMapper] Added 2 new mappings

--- PrivateSchool/MappingConfiguration.cs
+++ PrivateSchool/MappingConfiguration.cs
@@ -25,6 +25,21 @@ namespace PrivateSchool
             CreateMap<Teacher, TeacherReturnModel>()
                 .ForMember(dest => dest.SubjectName, opt => opt.MapFrom(src => src.Subject.Name))
                 .ForMember(dest=>dest.Username, opt => opt.MapFrom(src=>src.User.UserName));
+            CreateMap<Teacher, FullInfoTeacherModel>()
+                .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.User.DateOfBirth))
+                .ForMember(dest => dest.EGN, opt => opt.MapFrom(src => src.User.EGN))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.User.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.User.LastName))
+                .ForMember(dest => dest.Level, opt => opt.MapFrom(src => src.Level));
+            CreateMap<Student, FullInfoStudentModel>()
+               .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.User.DateOfBirth))
+               .ForMember(dest => dest.EGN, opt => opt.MapFrom(src => src.User.EGN))
+               .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
+               .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.User.FirstName))
+               .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.User.LastName))
+               .ForMember(dest => dest.AverageGrade, opt => opt.MapFrom(src => src.AverageGrade));
+
         }
 
     }
